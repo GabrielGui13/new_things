47283e71fb6b9a6313488736d35e26a8f879fd9e
Gabriel Guilherme <gabrielguilherme13@hotmail.com>
2022-01-12 10:30:39 -0300

    q3 finalizada

--- Cpp-Multiprova/12-01-22/q3/program.cpp
+++ Cpp-Multiprova/12-01-22/q3/program.cpp
@@ -8,7 +8,13 @@ struct Bairro {
     char cidade[50];
 };
 
+struct Cidade {
+    char nome[50];
+};
+
 void receberBairros(Bairro bairros[], int N);
+bool procurarCidade(Cidade cidades[], Bairro bairro, int N);
+void mostrarBairros(Cidade cidades[], Bairro bairros[], int N);
 
 int main() {
     int N = 0;
@@ -19,11 +25,47 @@ int main() {
     Bairro bairros[N];
     receberBairros(bairros, N);
 
-    //criar um struct para a cidade
-    //criar um array de cidades para ter controle de cada uma
-    //caso a cidade ja exista na array, inserir valor ""
+    Cidade cidades[N];
+
+    for (int i = 0; i < N; i++) {
+        if (procurarCidade(cidades, bairros[i], N)) {
+            strcpy(cidades[i].nome, "");
+        }
+        else {
+            strcpy(cidades[i].nome, bairros[i].cidade);
+        }
+    }
+
+    mostrarBairros(cidades, bairros, N);
 
     return 0;
+}
+
+void mostrarBairros(Cidade cidades[], Bairro bairros[], int N) {
+    for (int i = 0; i < N; i++) {
+        int count = 0;
+        Bairro parBairros[2];
+
+        if (strcmp(cidades[i].nome, "") == 0) continue; 
+
+        for (int y = 0; y < N; y++) {
+            if (strcmp(cidades[i].nome, bairros[y].cidade) == 0) {
+                parBairros[count] = bairros[y];
+                count++;
+            }
+        }
+
+        cout << parBairros[0].nome << " e " << parBairros[1].nome << " sao de " << parBairros[0].cidade << endl;
+    }
+}
+
+bool procurarCidade(Cidade cidades[], Bairro bairro, int N) {
+    bool achou = false;
+    for (int i = 0; i < N; i++) {
+        if (strcmp(cidades[i].nome, bairro.cidade) == 0) achou = true;
+    }
+
+    return achou;
 }
 
 void receberBairros(Bairro bairros[], int N) {
